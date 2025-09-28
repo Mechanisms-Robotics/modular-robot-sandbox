b10f8d4a144333a39659384683a7a8dd0dfafa17
Nolan Martzall <Nolan.M@themechanisms.org>
2025-09-28 11:13:50 -0400

    Gyro Zero Button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,9 +10,12 @@ import frc.robot.subsystems.DrivetrainController;
 import frc.robot.subsystems.PoseEstimator8736;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -50,6 +53,13 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+
+    controller.L3().onTrue(new InstantCommand(
+      () -> {
+        poseEstimator.zeroGyro();
+      }
+    ));
+
     drivetrain.setDefaultCommand(
       new RunCommand(
           () -> {
