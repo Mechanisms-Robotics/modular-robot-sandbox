de04c4f81f866596c593e2123efc12a96b9caf61
Talia Fox <taliareillyfox@gmail.com>
2025-09-30 20:50:37 -0400

    started outtake motor creations

---
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+
+public class Outtake extends SubsystemBase {
+
+    //update this CAN ID to match the hardware
+    private static final int OUTTAKE_MOTOR_CAN_ID = 20;
+
+    private static final SparkMax motor = new SparkMax(OUTTAKE_MOTOR_CAN_ID, MotorType.kBrushless);
+
+    /** Creates a new Outtake. */
+    public Outtake() {
+
+    }
+
+    //TODO: add a set method to make the motor go brrr and stop
+    //TODO: add an outtake command 
+    //TODO: add a stop outtake command
+    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+}
